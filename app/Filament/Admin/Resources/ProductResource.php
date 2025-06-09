9e699ad985f3c08e80ee52e7a974b48ff9f824bd
ghirsan <gamersid15@gmail.com>
2025-06-09 18:33:12 +0700

    fix: [ga] add Product Resource

---
+++ app/Filament/Admin/Resources/ProductResource.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\ProductResource\Pages;
+use App\Filament\Admin\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use FilamentTiptapEditor\TiptapEditor;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function getModelLabel(): string
+    {
+        return 'Produk';
+    }
+
+    public static function getPluralModelLabel(): string
+    {
+        return 'Produk';
+    }
+
+    public static function getNavigationGroup(): ?string
+    {
+        return 'Produk';
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                ->required()
+                ->maxLength(255),
+
+                TextInput::make('slug')
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(ignoreRecord: true),
+
+                TagsInput::make('product_category_ids')
+                    ->label('Category IDs'),
+
+                FileUpload::make('images')
+                    ->multiple()
+                    ->required()
+                    ->image(),
+
+                TiptapEditor::make('description')
+                    ->required(),
+
+                TextInput::make('retail_price')
+                    ->numeric()
+                    ->prefix('Rp')
+                    ->suffix('.00'),
+
+                Repeater::make('wholesale_prices')
+                    ->schema([
+                        TextInput::make('min_qty')->numeric()->required(),
+                        TextInput::make('price')->numeric()->required()->prefix('Rp'),
+                    ])
+                    ->label('Wholesale Prices'),
+
+                TextInput::make('shopee_link')
+                    ->label('Shopee Link')
+                    ->url()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('order')->label('Urutan')->sortable(),
+                Tables\Columns\TextColumn::make('name')->label('Nama')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('slug')->label('Slug')->searchable(),
+                Tables\Columns\TextColumn::make('created_at')->dateTime('d M Y H:i')->label('Dibuat')->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
